c98c87531f662be9caabb345b2f218237157df92
jcl <chaelynjang@lotte.net>
2024-10-02 15:57:29 +0900

    커스텀 incrementer 적용

---
+++ src/main/java/io/spring/springbatch/simplejob/IncrementerConfiguration.java
@@ -0,0 +1,50 @@
+package io.spring.springbatch.simplejob;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class IncrementerConfiguration {
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+
+    @Bean
+    public Job incrementerJob() {
+        return new JobBuilder("incrementerJob", jobRepository)
+                .start(incrementerStep1())
+                .next(incrementerStep2())
+                .incrementer(new CustomJobParametersIncrementer())
+                .build();
+    }
+
+    @Bean
+    public Step incrementerStep1() {
+        return new StepBuilder("incrementerStep1", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("incrementerStep1 has executed");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step incrementerStep2() {
+        return new StepBuilder("incrementerStep2", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("incrementerStep2 has executed");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+}
