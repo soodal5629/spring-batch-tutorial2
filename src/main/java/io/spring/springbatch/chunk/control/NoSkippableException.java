17f6b6c861efbc3c9e8824adff5d6748a2b3abde
jcl <chaelynjang@lotte.net>
2024-11-21 14:26:37 +0900

    SkipPolicy, 스킵하지 않을 예외 클래스 등 직접 설정

---
+++ src/main/java/io/spring/springbatch/chunk/control/NoSkippableException.java
@@ -0,0 +1,7 @@
+package io.spring.springbatch.chunk.control;
+
+public class NoSkippableException extends Exception {
+    public NoSkippableException(String s) {
+        super(s);
+    }
+}
--- src/main/java/io/spring/springbatch/chunk/control/SkipConfiguration.java
+++ src/main/java/io/spring/springbatch/chunk/control/SkipConfiguration.java
@@ -7,10 +7,15 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.skip.LimitCheckingItemSkipPolicy;
+import org.springframework.batch.core.step.skip.SkipPolicy;
 import org.springframework.batch.item.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -44,9 +49,24 @@ public class SkipConfiguration {
                 .processor(sipItemProcessor())
                 .writer(skipItemWriter())
                 .faultTolerant()
-                .skip(SkippableException.class)
-                .skipLimit(2)
+//                .skip(SkippableException.class)
+//                .skipLimit(2)
+                .skipPolicy(limitCheckingItemSkipPolicy())
+                .noSkip(NoSkippableException.class) // 해당 예외가 발생하면 skip X
+                .allowStartIfComplete(true)
                 .build();
+    }
+
+    /**
+     * 커스텀하게 SkipPolicy 설정 가능
+     */
+    @Bean
+    public SkipPolicy limitCheckingItemSkipPolicy() {
+        Map<Class<? extends Throwable>, Boolean> exceptionClass = new HashMap<>();
+        exceptionClass.put(SkippableException.class, true);
+        LimitCheckingItemSkipPolicy skipPolicy = new LimitCheckingItemSkipPolicy(3, exceptionClass);
+
+        return skipPolicy;
     }
 
     @Bean
