81fdfe67f7fceeccade0778146ff7868f118caca
jcl <chaelynjang@lotte.net>
2024-11-26 10:41:57 +0900

    커스텀 RetryTemplate의 execute 메소드에 RetryStatus 파라미터를 추가함으로써 기존의 RetryTemplate과 동일하게 동작하도록 설정

--- src/main/java/io/spring/springbatch/chunk/control/retrytemplate/RetryTemplateItemProcessor.java
+++ src/main/java/io/spring/springbatch/chunk/control/retrytemplate/RetryTemplateItemProcessor.java
@@ -2,6 +2,11 @@ package io.spring.springbatch.chunk.control.retrytemplate;
 
 import io.spring.springbatch.chunk.control.RetryableException;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.classify.BinaryExceptionClassifier;
+import org.springframework.classify.Classifier;
+import org.springframework.retry.RecoveryCallback;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.support.DefaultRetryState;
 import org.springframework.retry.support.RetryTemplate;
 
 public class RetryTemplateItemProcessor implements ItemProcessor<String, Item> {
@@ -15,17 +20,25 @@ public class RetryTemplateItemProcessor implements ItemProcessor<String, Item> {
 
     @Override
     public Item process(String item) throws Exception {
+        Classifier<Throwable, Boolean> rollbackClassifier = new BinaryExceptionClassifier(true);
+
         Item returnItem = customRetryTemplate.execute(
                 // doWithRetry 메소드
                 retryContext -> {
-                    if(item.equals("1") || item.equals("2")) {
+                    if (item.equals("1") || item.equals("2")) {
                         cnt++;
                         throw new RetryableException("failed cnt = " + cnt);
                     }
                     return new Item(item);
                 },
                 // recover 메소드
-                retryContext -> new Item(item));
+                new RecoveryCallback<Item>() {
+                    @Override
+                    public Item recover(RetryContext retryContext) throws Exception {
+                        return new Item(item);
+                    }
+                }, new DefaultRetryState(item, rollbackClassifier)
+        );
         return returnItem;
     }
 }
