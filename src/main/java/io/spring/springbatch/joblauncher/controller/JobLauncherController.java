7237128b0489b2c5c365cdef6fc66b6ff79b7a13
jcl <chaelynjang@lotte.net>
2024-12-06 13:14:08 +0900

    chore: 불필요 코드 삭제

--- src/main/java/io/spring/springbatch/joblauncher/controller/JobLauncherController.java
+++ src/main/java/io/spring/springbatch/joblauncher/controller/JobLauncherController.java
@@ -8,13 +8,13 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+
 import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
